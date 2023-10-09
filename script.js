3b39d47848629ed1201d9f6e4265cf64f8d74e97
musa <m.abdulrofihi@gmail.com>
2023-10-09 13:35:31 +0100

    Complete UI logic The script is completed and ready to go. I'll beautify the page later.

--- script.js
+++ script.js
@@ -18,21 +18,36 @@ options.addEventListener('click', (event) => {
     switch(target.id) {
         case 'rock':
             playRound('rock', getComputerChoice());
+            scoreBoard();
             break;
         case 'paper':
             playRound('paper', getComputerChoice());
+            scoreBoard();
             break;
         case 'scissors':
             playRound('scissors', getComputerChoice());
+            scoreBoard();
             break;
     }
 });
 
 const result = document.querySelector('.result');
+const playerScoreBoard = document.querySelector('.player-score');
+const computerScoreBoard = document.querySelector(".computer-score");
 
 // score board
 let playerScore = 0;
 let computerScore = 0;
+
+function scoreBoard () {
+    if (playerScore === 5 && computerScore < 5) {
+        result.textContent = "You win this round!";
+        console.log(playerScore);
+    } else if (computerScore === 5 && playerScore < 5) {
+        result.textContent = "You lose this round. The matrix wins!";
+        console.log(computerScore);
+    }
+}
 
 // rock crushes scissors
 // paper covers rock
@@ -43,33 +58,57 @@ function playRound(playerSelection, computerSelection) {
 
     if (playerSelection === "rock") {
         if (computerSelection === "scissors") {
+            // "You win! Rock crushes Scissors."
             playerScore++;
-            result.textContent = "You win! Rock crushes Scissors.";
+
+            playerScoreBoard.textContent = `Score (Player): ${playerScore}`;
+            computerScoreBoard.textContent = `Score (Matrix): ${computerScore}`;
         } else if (computerSelection === "paper") {
+            // "You lose! Paper covers Rock."
             computerScore++;
-            result.textContent = "You lose! Paper covers Rock.";
+
+            playerScoreBoard.textContent = `Score (Player): ${playerScore}`;
+            computerScoreBoard.textContent = `Score (Matrix): ${computerScore}`;
         } else {
-            result.textContent = "It is a tie!";
+            // Tie!
+            playerScoreBoard.textContent = `Score (Player): ${playerScore}`;
+            computerScoreBoard.textContent = `Score (Matrix): ${computerScore}`;
         }
     } else if (playerSelection === "paper") {
         if (computerSelection === "rock") {
+            // "You win! Paper covers Rock."
             playerScore++;
-            result.textContent = "You win! Paper covers Rock.";
+
+            playerScoreBoard.textContent = `Score (Player): ${playerScore}`;
+            computerScoreBoard.textContent = `Score (Matrix): ${computerScore}`;
         } else if (computerSelection === "scissors") {
+            // "You lose! Scissors cuts Paper."
             computerScore++;
-            result.textContent = "You lose! Scissors cuts Paper.";
+
+            playerScoreBoard.textContent = `Score (Player): ${playerScore}`;
+            computerScoreBoard.textContent = `Score (Matrix): ${computerScore}`;
         } else {
-            result.textContent = "It is a tie!";
+            //  Tie
+            playerScoreBoard.textContent = `Score (Player): ${playerScore}`;
+            computerScoreBoard.textContent = `Score (Matrix): ${computerScore}`;
         }
     } else if (playerSelection === "scissors") {
         if (computerSelection === "paper") {
+            // "You win! Scissors cuts Paper."
             playerScore++;
-            result.textContent = "You win! Scissors cuts Paper.";
+
+            playerScoreBoard.textContent = `Score (Player): ${playerScore}`;
+            computerScoreBoard.textContent = `Score (Matrix): ${computerScore}`;
         } else if (computerSelection === "rock") {
+            // "You lose! Rock crushes Scissors."
             computerScore++;
-            result.textContent = "You lose! Rock crushes Scissors.";
+
+            playerScoreBoard.textContent = `Score (Player): ${playerScore}`;
+            computerScoreBoard.textContent = `Score (Matrix): ${computerScore}`;
         } else {
-            result.textContent = "It is a tie!";
+            //  Tie
+            playerScoreBoard.textContent = `Score (Player): ${playerScore}`;
+            computerScoreBoard.textContent = `Score (Matrix): ${computerScore}`;
         }
     } else {
         result.textContent = "Your choice is not valid :(";
